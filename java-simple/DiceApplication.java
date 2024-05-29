cf40cf81d58ea25ea2466ba594680ebbb5f47dce
2115-27 <103985748+2115-27@users.noreply.github.com>
2024-05-29 12:15:17 +0530

    Update DiceApplication.java

--- java-simple/DiceApplication.java
+++ java-simple/DiceApplication.java
@@ -1,3 +1,4 @@
+// DiceApplication.java
 package otel;
 
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiceApplication {
   public static void main(String[] args) {
+
     SpringApplication app = new SpringApplication(DiceApplication.class);
     app.setBannerMode(Banner.Mode.OFF);
     app.run(args);
